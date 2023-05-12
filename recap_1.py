72094aa34fc6ffa57e45b907c8dded4207d99a36
maeh98 <EHRLICHMANUEL@GMAIL.COM>
2023-05-12 11:02:08 +0200

    commentato

--- recap_1.py
+++ recap_1.py
@@ -1,11 +1,17 @@
+# Parola
 stringa = "banana"
 
+# Inizializza
 dizionario = {}
 
+# Cicla sulle lettere della parola
 for lettera in stringa:
+    # Aggiungi lettera al dizionario se non è presente
     if lettera not in dizionario:
         dizionario[lettera] = 1
     else:
+        # Se è già presente aumenta il conteggio
         dizionario[lettera] += 1
 
+# Printa
 print(dizionario.items())
